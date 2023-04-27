c4012b27f853a42ecd3df1994bef5fc5ebd1a7e9
Gustavo Ribeiro <50669251+devGustavoR@users.noreply.github.com>
2023-04-27 18:28:21 -0300

    Melhorias adicionadas

--- sorteador.py
+++ sorteador.py
@@ -1,40 +1,31 @@
 # Importações
 import random
 
-jogadores = ['Igor', 'Gustavo', 'Eros', 'Cassio', 'Nicolas', 'Marlon','Icaro']
+# Adicione o nome de cada jogador
+jogadores = ['Jogador1', 'Jogador2', 'Jogador3', 'Jogador4', 'Jogador5', 'Jogador6','Jogador7']
+
+# Lista de insignias para cada geração
+insignias = [
+    # Geração 1
+    ['Boulder Badge', 'Cascade Badge', 'Thunder Badge', 'Rainbow Badge', 'Marsh Badge', 'Soul Badge', 'Volcano Badge', 'Earth Badge'],
+    # Geração 2
+    ['Zephyr Badge', 'Hive Badge', 'Plain Badge', 'Fog Badge', 'Storm Badge', 'Mineral Badge', 'Glacier Badge', 'Rising Badge'],
+    # Geração 3
+    ['Stone Badge', 'Knuckle Badge', 'Dynamo Badge', 'Heat Badge', 'Balance Badge', 'Feather Badge', 'Mind Badge', 'Rain Badge'],
+    # Geração 4
+    ['Coal Badge', 'Forest Badge', 'Cobble Badge', 'Fen Badge', 'Relic Badge', 'Mine Badge', 'Icicle Badge', 'Beacon Badge'],
+    # Geração 5
+    ['Basic Badge', 'Insect Badge', 'Bolt Badge', 'Quake Badge', 'Jet Badge', 'Freeze Badge', 'Legend Badge', 'Toxic Badge', 'Wave Badge', 'Trio Badge'],
+    # Geração 6
+    ['Bug Badge', 'Cliff Badge', 'Rumble Badge', 'Plant Badge', 'Voltage Badge', 'Fairy Badge', 'Psychic Badge', 'Iceberg Badge']
+]
+
+# Gerar número aleatório de 1 a 6 para representar a geração
 geracao = random.randint(1, 6)
 
-# Insignias
-insignias1 = ['Boulder Badge','Cascade Badge','Earth Badge','Marsh Badge','Rainbow Badge'
-,'Soul Badge','Thunder Badge','Volcano Badge']
+# Exibir o jogador sorteado
+print('Jogador: ' + jogadores[random.randrange(len(jogadores))])
 
-insignias2 = ['Zephyr Badge','Hive Badge','Plain Badge','Fog Badge','Storm Badge','Mineral Badge'
-,'Glacier Badge','Rising Badge']
-
-insignias3 = ['Stone Badge','Knuckle Badge','Dynamo Badge','Heat Badge','Balance Badge','Feather Badge'
-,'Mind Badge','Rain Badge']
-
-insignias4 = ['Coal Badge','Forest Badge','Cobble Badge','Fen Badge','Relic Badge','Mine Badge'
-,'Icicle Badge','Beacon Badge']
-
-insignias5 = ['Trio Badge','Basic Badge','Insect Badge','Bolt Badge','Quake Badge	'
-,'Jet Badge','Freeze Badge','Legend Badge','Toxic Badge','Wave Badge']
-
-insignias6 = ['Bug Badge','Cliff Badge','Rumble Badge','Plant Badge','Voltage Badge'
-,'Fairy Badge','Psychic Badge','Iceberg Badge']
-
-
-print ('Jogador: ' + jogadores [ random.randrange ( len ( jogadores ))] )
-print('Geracao: {}'.format(geracao))
-if(geracao == 1):
-  print('Insignia: ' + insignias1 [ random.randrange ( len ( insignias1 ))])
-if(geracao==2):
-  print('Insignia: ' + insignias2 [ random.randrange ( len ( insignias2 ))])
-if(geracao==3):
-  print('Insignia: ' + insignias3 [ random.randrange ( len ( insignias3 ))])
-if(geracao==4):
-  print('Insignia: ' + insignias4 [ random.randrange ( len ( insignias4 ))])
-if(geracao==5):
-  print('Insignia: ' + insignias5 [ random.randrange ( len ( insignias5 ))])
-if(geracao==6):
-  print('Insignia: ' + insignias6 [ random.randrange ( len ( insignias6 ))])
+# Exibir a geração e a insígnia sorteada
+print('Geração: {}'.format(geracao))
+print('Insignia: ' + insignias[geracao-1][random.randrange(len(insignias[geracao-1]))])
